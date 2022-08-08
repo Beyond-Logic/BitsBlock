60cee77b88dc3e2b369e588dc465dbfc1a9950e0
Edema <edemaero@gmail.com>
2022-08-08 10:02:15 +0100

    update

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -131,7 +131,6 @@ const Welcome = () => {
             />
 
             <div className="h-[1px] w-full bg-gray-400 my-2" />
-
             {isLoading ? (
               <Loader />
             ) : (
